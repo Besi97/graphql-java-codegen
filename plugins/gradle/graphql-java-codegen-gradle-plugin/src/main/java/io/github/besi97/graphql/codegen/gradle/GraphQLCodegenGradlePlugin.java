5924481817aa9f08f0e53c6ce531ec6fef7526db
Dávid Besenyei <david.besenyei97@gmail.com>
2026-08-04 15:16:53 +0000

    Fix Task.getProject() deprecation at execution time
    Inject ProjectLayout service instead of calling getProject() during task
    execution to comply with configuration cache requirements and avoid Gradle
    10.0 deprecation error.
    
    Closes: #37

--- plugins/gradle/graphql-java-codegen-gradle-plugin/src/main/java/io/github/besi97/graphql/codegen/gradle/GraphQLCodegenGradlePlugin.java
+++ plugins/gradle/graphql-java-codegen-gradle-plugin/src/main/java/io/github/besi97/graphql/codegen/gradle/GraphQLCodegenGradlePlugin.java
@@ -2,7 +2,11 @@ package io.github.besi97.graphql.codegen.gradle;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.TaskContainer;
+
+import java.io.File;
 
 /**
  * Gradle plugin for GraphQL code generation
@@ -14,6 +18,22 @@ public class GraphQLCodegenGradlePlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         TaskContainer tasks = project.getTasks();
+        
+        File defaultResourcesDir = project.getExtensions()
+                .getByType(JavaPluginExtension.class)
+                .getSourceSets()
+                .getByName(SourceSet.MAIN_SOURCE_SET_NAME)
+                .getResources()
+                .getSourceDirectories()
+                .getFiles()
+                .stream()
+                .findFirst()
+                .orElse(null);
+        
+        tasks.withType(GraphQLCodegenGradleTask.class).configureEach(task -> {
+            task.setDefaultResourcesDir(defaultResourcesDir);
+        });
+        
         tasks.create("graphqlCodegen", GraphQLCodegenGradleTask.class);
         tasks.create("graphqlCodegenValidate", GraphQLCodegenValidateGradleTask.class);
     }
